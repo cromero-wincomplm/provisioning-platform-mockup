4e466ea8b14bd9d203ed8685acbf31f4279b0fba
cromero-wincomplm <cromero@wincom-plm.com>
2023-07-26 16:56:16 +0200

    Fixed components logic for SideBar

--- features/reactCDN/components/Body.js
+++ features/reactCDN/components/Body.js
@@ -1,11 +1,11 @@
 import '../css/base.css';
-import SideBar from './components/SideBar'
-
+import SideBar from './components/sidebar/SideBar'
+import ContentArea from '../components/sidebar/ContentArea';
 
 function Body() {
     return (
         <div id="mockup-body" className="mw-100">
-            <SideBar />
+            <ContentArea />
         </div>
     );
 
